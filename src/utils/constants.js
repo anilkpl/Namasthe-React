cca177570ff183aaf6b3ee46eb712c81baac66dd
anilkpl <102033847+anilkpl@users.noreply.github.com>
2023-09-25 10:31:21 +0530

    Ep-6 changes committed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,5 @@ export const RES_LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd
 
 export const RES_API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-export const RES_IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+export const RES_IMG_URL = 
+"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
