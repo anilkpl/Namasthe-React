67391687dd6f945aac19af9b5d60dcc1a8422b54
anilkpl <102033847+anilkpl@users.noreply.github.com>
2023-09-26 16:22:46 +0530

    Ep-9 changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,38 @@
 import { NavLink } from "react-router-dom";
 import { RES_LOGO_URL } from "../utils/constants";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
+  const onlineStatus = useOnlineStatus()
   return (
-    <div className="head-container">
-      <div className="logo-container">
-        <NavLink to="/">
-          <img alt="logo" className="logo-img" src={RES_LOGO_URL} />
-        </NavLink>
+    <>
+      <div className="head-container">
+        <div className="logo-container">
+          <NavLink to="/">
+            <img alt="logo" className="logo-img" src={RES_LOGO_URL} />
+          </NavLink>
+        </div>
+        
+        <div className="nav-container">
+          <ul className="nav-items">
+            
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+            <li>
+              <NavLink to="/cart">Cart</NavLink>
+            </li>
+            <li>{onlineStatus ? '✅' : '🔴'}</li>
+          </ul>
+        </div>
       </div>
-      <div className="nav-container">
-        <ul className="nav-items">
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-          <li>
-            <NavLink to="/cart">Cart</NavLink>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Shimmer from './Shimmer';
 
 export default class User extends Component {
     constructor(props){
@@ -10,11 +11,11 @@ export default class User extends Component {
     async componentDidMount(){
         const res = await fetch("https://api.github.com/users/anilkpl");
         const data = await res.json()
-        console.log(data)
+        // console.log(data)
         this.setState({data : data})
     }
   render() {
-    if(this.state.data === null) return;
+    if(this.state.data === null) return <Shimmer />;
     const {avatar_url,name} = this.state.data
     return (
      <div className='user-container'>
